be788f446e2b93eede3966762d0e128598f34da3
Anif07 <mahammadanif22@gmail.com>
2024-06-28 09:06:32 +0530

    implemented lazy loading

--- src/App.js
+++ src/App.js
@@ -1,62 +1,83 @@
-import { Route, Router, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
-import Packages from "./pages/Packages";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router";
 import Navbar from "./components/common/Navbar";
 import "./styles/global.css";
-import SinglePackage from "./components/Packages/SinglePackage";
-import PackageBilling from "./components/Packages/PackageBilling";
-import MyTrips from "./pages/MyTrips";
-import PackageBookingConfirm from "./components/Packages/PackageBookingConfirm";
-import Flights from "./pages/Flights";
-import SearchedFlights from "./components/Flights/SearchedFlights";
-import FlightBookingDetails from "./components/Flights/FlightBookingDetails";
-import MyPackages from "./components/MyTripsPages/MyPackages";
-import MyFlights from "./components/MyTripsPages/MyFlights";
-import MyHotels from "./components/MyTripsPages/MyHotels";
-import HotelPage from "./pages/HotelPage";
-import AllHotels from "./components/Hotels/AllHotels";
-import SingleHotelDetails from "./components/Hotels/singleHotelDetails";
-import HotelBilling from "./components/Hotels/HotelBilling";
-import HotelBookingConfirm from "./components/Hotels/hotelBookingConfirm";
-import BookingCompletedFlight from "./components/Flights/BookingCompletedFlight";
-import Signup from "./components/common/Signup";
-import LogIn from "./components/common/LogIn";
+
+// Lazy-loaded components
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const Packages = React.lazy(() => import("./pages/Packages"));
+const SinglePackage = React.lazy(() =>
+  import("./components/Packages/SinglePackage")
+);
+const PackageBilling = React.lazy(() =>
+  import("./components/Packages/PackageBilling")
+);
+const MyTrips = React.lazy(() => import("./pages/MyTrips"));
+const PackageBookingConfirm = React.lazy(() =>
+  import("./components/Packages/PackageBookingConfirm")
+);
+const Flights = React.lazy(() => import("./pages/Flights"));
+const SearchedFlights = React.lazy(() =>
+  import("./components/Flights/SearchedFlights")
+);
+const FlightBookingDetails = React.lazy(() =>
+  import("./components/Flights/FlightBookingDetails")
+);
+const HotelPage = React.lazy(() => import("./pages/HotelPage"));
+const AllHotels = React.lazy(() => import("./components/Hotels/AllHotels"));
+const SingleHotelDetails = React.lazy(() =>
+  import("./components/Hotels/singleHotelDetails")
+);
+const HotelBilling = React.lazy(() =>
+  import("./components/Hotels/HotelBilling")
+);
+const HotelBookingConfirm = React.lazy(() =>
+  import("./components/Hotels/hotelBookingConfirm")
+);
+const BookingCompletedFlight = React.lazy(() =>
+  import("./components/Flights/BookingCompletedFlight")
+);
+const Signup = React.lazy(() => import("./components/common/Signup"));
+const LogIn = React.lazy(() => import("./components/common/LogIn"));
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/packages" element={<Packages />} />
-        <Route path="/SinglePackage" element={<SinglePackage />} />
-        <Route path="/packageBilling" element={<PackageBilling />} />
-        <Route path="/myTrips/*" element={<MyTrips />} />
-        <Route
-          path="packageBookingConfirm"
-          element={<PackageBookingConfirm />}
-        />
-        <Route path="/Flights" element={<Flights />} />
-        <Route path="/SearchedFlights" element={<SearchedFlights />} />
-        <Route
-          path="/FlightBookingDetails"
-          element={<FlightBookingDetails />}
-        />
-        <Route path="/Hotels" element={<HotelPage />} />
-        <Route path="/AllHotels" element={<AllHotels />} />
-        <Route path="/HotelDetails" element={<SingleHotelDetails />} />
-        <Route path="/HotelBilling" element={<HotelBilling />} />
-        <Route path="/HotelBookingConfirm" element={<HotelBookingConfirm />} />
-        <Route
-          path="/FlightBookingCompleted"
-          element={<BookingCompletedFlight />}
-        />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/Login" element={<LogIn />} />
-        {/* <Route path="/myPackages" element={<MyPackages />} />
-        <Route path="/myFlights" element={<MyFlights />} />
-        <Route path="/myHotels" element={<MyHotels />} /> */}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/packages" element={<Packages />} />
+          <Route path="/SinglePackage" element={<SinglePackage />} />
+          <Route path="/packageBilling" element={<PackageBilling />} />
+          <Route path="/myTrips/*" element={<MyTrips />} />
+          <Route
+            path="/packageBookingConfirm"
+            element={<PackageBookingConfirm />}
+          />
+          <Route path="/Flights" element={<Flights />} />
+          <Route path="/SearchedFlights" element={<SearchedFlights />} />
+          <Route
+            path="/FlightBookingDetails"
+            element={<FlightBookingDetails />}
+          />
+          <Route path="/Hotels" element={<HotelPage />} />
+          <Route path="/AllHotels" element={<AllHotels />} />
+          <Route path="/HotelDetails" element={<SingleHotelDetails />} />
+          <Route path="/HotelBilling" element={<HotelBilling />} />
+          <Route
+            path="/HotelBookingConfirm"
+            element={<HotelBookingConfirm />}
+          />
+          <Route
+            path="/FlightBookingCompleted"
+            element={<BookingCompletedFlight />}
+          />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Login" element={<LogIn />} />
+          {/* Additional routes commented out for clarity */}
+        </Routes>
+      </Suspense>
     </div>
   );
 }
